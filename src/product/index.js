a770a8bf3b6c249a8394a946e1d092c3a7d8250a
kai <kty14037@gmail.com>
2022-11-17 23:21:32 +0900

    상세 페이지1

--- src/product/index.js
+++ src/product/index.js
@@ -1,7 +1,23 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://927cadea-9216-4713-9533-3f224ddf6955.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
   return <h1>상품 상세 페이지 {id} 상품</h1>;
 }
 
