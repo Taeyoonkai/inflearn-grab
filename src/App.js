4393f52a50b1bb371095356bc69b66e08af0dcdf
kty14037 <kty14037@gmail.com>
2022-11-18 19:09:13 +0900

    Antd적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import MainPageComponent from "./main/index.js";
 import "./App.css";
+import MainPageComponent from "./main";
+import { Switch, Route, Link } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
+import "antd/dist/antd.css";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
 }
+
 export default App;
