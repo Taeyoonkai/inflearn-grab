f0f462b55a484665c1ffd34af71822af15ae1eb6
kai <kty14037@gmail.com>
2022-11-22 18:31:22 +0900

    cors업로드

--- src/main/index.js
+++ src/main/index.js
@@ -7,9 +7,7 @@ function MainPage() {
   const [products, setProducts] = React.useState([]);
   React.useEffect(function () {
     axios
-      .get(
-        "https://927cadea-9216-4713-9533-3f224ddf6955.mock.pstmn.io/products"
-      )
+      .get("http://localhost:8080/products/")
       .then(function (result) {
         const products = result.data.products;
         setProducts(products);
